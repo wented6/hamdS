5c6f4542e58f716ff6b8227c0e788b0a76ac4674
wented6 <44713416+wented6@users.noreply.github.com>
2019-01-22 22:37:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,19 +3,26 @@ const client = new Discord.Client();
 const developers = ['321960094101143552'];
 
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setStatus("dnd")
-});
-
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!developers.includes(message.author.id)) return;
 
+if (message.content.startsWith('.sggggggggggggggggggggg')) {
+client.user.setActivity(argresult, {type:'Playing'});
+    message.channel.send(` تم تغير البلاينق ألى **${argresult} \** `)
+} else 
 if (message.content.startsWith('!D')) {
   client.user.setGame(argresult, "https://www.twitch.tv/meercy");
-   message.channel.send(` < **${argresult} \** `)
+   message.channel.send(` تم تغير الستريمنق ألى **${argresult} \** `)
 } else
-
+if (message.content.startsWith('.sggggggggggggggggggggg')) {
+   client.user.setActivity(argresult, {type:'Listening'});
+    message.channel.send(` تم تغير اللسننق ألى **${argresult} \** `)
+} else
+if (message.content.startsWith('.sggggggggggggggggggggg')) {
+   client.user.setActivity(argresult, {type:'Watching'});
+    message.channel.send(` تم تغير الواتشنق ألى **${argresult} \** `)
+}
+});
 
 client.login(process.env.B);
